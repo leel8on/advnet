1682086a2d969e9612736eda0c7439b487f8b17a
TheCmar7 <runner.cram@gmail.com>
2018-03-05 13:49:56 -0800

    added a simple sample mean project and a str value for the Stats object

--- getstats.py
+++ getstats.py
@@ -11,6 +11,10 @@ blist = []
 # HTTP 2 With TLS
 clist = []
 
+
+
+
+
 class Stats:
     def __init__(self, numc, tc, tp, tt):
         self.num_connects = numc
@@ -18,6 +22,30 @@ class Stats:
         self.time_pretransfer = tp
         self.time_total = tt
 
+    def __str__(self):
+    	print("Stats ID:" + str(id(self)))
+    	return ' #_conn: %d\n t_conn: %f\n t_prtf: %f\n t_totl: %f' % (
+    		self.num_connects, 
+    		self.time_connect, 
+    		self.time_pretransfer, 
+    		self.time_total);
+
+def SampleMean(ls):
+	conn_sum = 0.0
+	time_sum = 0.0
+	prtm_sum = 0.0
+	totm_sum = 0.0
+	for l in ls:
+		print(l)
+		conn_sum += l.num_connects 
+		time_sum += l.time_connect
+		prtm_sum += l.time_pretransfer
+		totm_sum += l.time_total
+
+	return Stats(conn_sum / len(ls), 
+				 time_sum / len(ls), 
+				 prtm_sum / len(ls), 
+				 totm_sum / len(ls))
 
 if __name__ == "__main__":
     f = open("/dev/null", "w")
@@ -64,6 +92,8 @@ if __name__ == "__main__":
             c.getinfo(c.PRETRANSFER_TIME),
             c.getinfo(c.TOTAL_TIME)))
 
-
+    print(SampleMean(alist));
+    print(SampleMean(blist));
+    print(SampleMean(clist));
 
     f.close()
