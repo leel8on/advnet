77d007d2cbbbf7436588c83acb3e819183561997
Lee Leighton <leel8on@osuosl.org>
2018-03-05 13:33:06 -0800

    Factor out the calling function

--- getstats.py
+++ getstats.py
@@ -2,14 +2,20 @@
 
 import pycurl
 
-# HTTP 1.1 No TLS
-alist = []
+# Object lists for text only site
+text1 = []
+text1tls = []
+text2 = []
 
-# HTTP 1.1 With TLS
-blist = []
+# Lists for single source image site
+img1 = []
+img1tls = []
+img2 = []
 
-# HTTP 2 With TLS
-clist = []
+# Lists for multi source image site
+multi1 = []
+multi1tls = []
+multi2 = []
 
 class Stats:
     def __init__(self, numc, tc, tp, tt):
@@ -19,51 +25,40 @@ class Stats:
         self.time_total = tt
 
 
-if __name__ == "__main__":
+def runstats(urlstr, version, lst):
     f = open("/dev/null", "w")
-
-    # HTTP 1.1 No TLS
     a = pycurl.Curl()
-    a.setopt(a.URL, 'http://advnet.l8on.org')
-    a.setopt(a.HTTP_VERSION, a.CURL_HTTP_VERSION_1_1)
+    a.setopt(a.URL, urlstr)
+    if version == 1:
+        a.setopt(a.HTTP_VERSION, a.CURL_HTTP_VERSION_1_1)
+    else:
+        a.setopt(a.HTTP_VERSION, a.CURL_HTTP_VERSION_2_0)
     a.setopt(a.WRITEDATA, f)
-
-    # HTTP 1.1 With TLS
-    b = pycurl.Curl()
-    b.setopt(b.URL, 'https://advnet.l8on.org')
-    b.setopt(b.HTTP_VERSION, b.CURL_HTTP_VERSION_1_1)
-    b.setopt(b.WRITEDATA, f)
-
-    # HTTP 2 With TLS
-    c = pycurl.Curl()
-    c.setopt(c.URL, 'https://advnet.l8on.org')
-    c.setopt(c.HTTP_VERSION, c.CURL_HTTP_VERSION_2_0)
-    c.setopt(c.WRITEDATA, f)
 
     for i in range(100):
         a.perform()
-        alist.append(Stats(
+        lst.append(Stats(
             a.getinfo(a.NUM_CONNECTS),
             a.getinfo(a.CONNECT_TIME),
             a.getinfo(a.PRETRANSFER_TIME),
             a.getinfo(a.TOTAL_TIME)))
 
-    for i in range(100):
-        b.perform()
-        blist.append(Stats(
-            b.getinfo(b.NUM_CONNECTS),
-            b.getinfo(b.CONNECT_TIME),
-            b.getinfo(b.PRETRANSFER_TIME),
-            b.getinfo(b.TOTAL_TIME)))
-
-    for i in range(100):
-        c.perform()
-        clist.append(Stats(
-            c.getinfo(c.NUM_CONNECTS),
-            c.getinfo(c.CONNECT_TIME),
-            c.getinfo(c.PRETRANSFER_TIME),
-            c.getinfo(c.TOTAL_TIME)))
-
-
-
     f.close()
+
+
+if __name__ == "__main__":
+
+    # All the variations of the sites
+    texturl = 'http://advnet.l8on.org'
+    texturltls = 'https://advnet.l8on.org'
+    imgurl = 'http://advnet.l8on.org:8080'
+    imgurltls = 'https://advnet.l8on.org:8081'
+    mimgurl = 'http://advnet.l8on.org:8082'
+    mimgurltls = 'https://advnet.l8on.org:8083'
+
+    runstats(texturl, 1, text1)
+    runstats(texturltls, 1, text1tls)
+    runstats(texturltls, 2, text2)
+
+    for i in range(len(text1)):
+        print text1[i].time_total
