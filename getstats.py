dd2f9ba43c77bbfafda1484e933be10cbaa0b13b
TheCmar7 <runner.cram@gmail.com>
2018-03-05 18:23:44 -0800

    it works still?

--- getstats.py
+++ getstats.py
@@ -26,13 +26,25 @@ text1tls_sm = []
 text2_sm = []
 
 img1_sm = []
-img1tls = []
-img2 = []
+img1tls_sm = []
+img2_sm = []
 
 multi1_sm = []
 multi1tls_sm = []
 multi2_sm = []
 
+#simulation runs
+text1_n = 0
+text1tls_n = 0
+text2_n = 0
+
+img1_n = 0
+img1tls_n = 0
+img2_n = 0
+
+multi1_n = 0
+multi1tls_n = 0
+multi2_n = 0
 
 # Stats Class 
 class Stats:
@@ -68,7 +80,7 @@ def SampleMean(ls):
 				 totm_sum / len(ls))
 
 # runstats
-def runstats(urlstr, version, lst, sm_lst):
+def runstats(urlstr, version, lst, sm_lst, n):
     f = open("/dev/null", "w")
     
     a = pycurl.Curl()
@@ -111,7 +123,7 @@ def runstats(urlstr, version, lst, sm_lst):
     #  	print(l)
 
     vari = numpy.var(total_time);
-    print(vari)
+    #print(vari)
 
     z_val = 1.645
 
@@ -120,7 +132,7 @@ def runstats(urlstr, version, lst, sm_lst):
 
     #while(numpy.float64(sm_lst[-1].time_total * .1) <= conf):
     while (conf > .001):
-    	print(str(conf) + " > " + str(.001))
+    	#print(str(conf) + " > " + str(.0015))
     	a = pycurl.Curl()
         a.setopt(a.URL, urlstr)
         if version == 1:
@@ -141,6 +153,8 @@ def runstats(urlstr, version, lst, sm_lst):
         conf = (2 * math.sqrt(numpy.var(total_time)) * 1.645) / (math.sqrt(len(total_time)));
        # print(str(conf) + " > " + str(numpy.float64(sm_lst[-1].time_total * .1)))
 
+    print("took: " + str(len(total_time)));
+    n = len(total_time);
     f.close()
 
 if __name__ == "__main__":
@@ -155,22 +169,57 @@ if __name__ == "__main__":
 
 
     # Text only sites
-    runstats(texturl, 1, text1, text1_sm)
-    runstats(texturltls, 1, text1tls, text1tls_sm )
-    runstats(texturltls, 2, text2, text2_sm )
+    print("\ntext only")
+
+    print("--text1")
+    runstats(texturl, 1, text1, text1_sm, text1_n)
+    print("Runs: " + str(text1_n))
+    print("Sample Mean: " + str(SampleMean(text1)));
+    
+    print("--texturltls")
+    runstats(texturltls, 1, text1tls, text1tls_sm, text1tls_n)
+    print("Runs: " + str(text1tls_n))
+    print("Sample Mean: " + str(SampleMean(text1tls)));
+    
+    print("--texturltls 2")
+    runstats(texturltls, 2, text2, text2_sm, text2_n)
+    print("Runs: " + str(text2_n))
+    print("Sample Mean: " + str(SampleMean(text2)));
 
     # Single source image sites
-    runstats(imgurl, 1, img1, img1_sm)
-    runstats(imgurltls, 1, img1tls, img1_sm)
-    runstats(imgurltls, 2, img2, img1_sm)
+    print("\nimages from 1 source")
+
+    print("--imgurl")
+    runstats(imgurl, 1, img1, img1_sm, img1_n)
+    print("Runs: " + str(img1_n))
+    print("Sample Mean: " + str(SampleMean(img1)));
     
+    print("--imgurltls")
+    runstats(imgurltls, 1, img1tls, img1_sm, img1_n)
+    print("Runs: " + str(img1_n))
+    print("Sample Mean: " + str(SampleMean(img1tls)));
+    
+    print("--imgurltls 2")
+    runstats(imgurltls, 2, img2, img2_sm, img2_n)
+    print("Runs: " + str(img2_n))
+    print("Sample Mean: " + str(SampleMean(img2)));
+
     # Multi source image sites
-    runstats(mimgurl, 1, multi1, multi1_sm)
-    runstats(mimgurltls, 1, multi1tls, multi1tls_sm)
-    runstats(mimgurltls, 2, multi2, multi2_sm)
+    print("\nimages from 1 source")
+    
+    print("--mimgurl")
+    runstats(mimgurl, 1, multi1, multi1_sm, multi1_n)
+    print("Runs: " + str(multi1_n))
+    print("Sample Mean: " + str(SampleMean(multi1)));
+    
+    print("--mimgurltls")
+    runstats(mimgurltls, 1, multi1tls, multi1tls_sm, multi1tls_n)
+    print("Runs: " + str(multi1tls_n))
+    print("Sample Mean: " + str(SampleMean(multi1tls)));
+    
+    print("--mimgurltls 2")
+    runstats(mimgurltls, 2, multi2, multi2_sm, multi2_n)
+    print("Runs: " + str(multi2_n))
+    print("Sample Mean: " + str(SampleMean(multi2)));
  
-
-    print("Sample Mean: " + str(SampleMean(text1)));
-   # print("Sample Mean: " + str(SampleMean(img1)));
-   # print("Sample Mean: " + str(SampleMean(multi1)));
-
+    print("\nDONE!")
